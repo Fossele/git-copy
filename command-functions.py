5a500ae81669e6be33e35ff6d50330c0177f4ca4
Fossele <fosselewilfried@gmail.com>
2026-04-08 18:57:20 +0100

    created a file for the command functiions and factorise the the createGitRespository file

---
+++ command-functions.py
@@ -0,0 +1,10 @@
+def cmd_add(*file_name):
+                if file_name == ".":
+                 #stageAllFiles()
+                    return 0;
+                else:
+                    #stageFileName(file_name)
+                    pool = 0                    
+                    
+def cmd_commit():
+    print("create commit tree")
