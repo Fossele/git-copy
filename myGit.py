2e5d511a7fe81728e39cb4d0c3e663541185382e
Fossele <fosselewilfried@gmail.com>
2026-04-12 20:27:11 +0100

    working on the tree and commit tree object

--- myGit.py
+++ myGit.py
@@ -2,35 +2,54 @@ import hashlib
 import zlib
 import os
 
-content = b"Hello, mgit"
 
-def createblob(content):
+def convertfilecontentToBytes(file):
+    with open(file, "rb") as f:
+        content = f.read()
+  #  print("Successful conversion")
+    return content
+
+
+def createblob(file_name):
+    content = convertfilecontentToBytes(file_name)
     header = f"Blob {content}\x00".encode()
-    combinedata = header + content 
+    combinedata = header + content
     result = hashlib.sha1(combinedata).hexdigest()
     obj_dir = f".mgit/objects/{result[2:]}"
-    obj_path = f"{obj_dir}/{result[2:]}"
+    obj_path = f"{obj_dir}/{result[:2]}"
     os.makedirs(obj_dir, exist_ok=True)
     with open(obj_path, "wb") as f:
         f.write(zlib.compress(combinedata))
-    
-createblob(b"Hello Mga")
+    print("blob successfully created")
+    print(obj_path)
 
+
+def createEntries(directory):
+    if os.path.isfile(directory): 
+        print("make a node out of")
+        createblob(directory)
+        print(directory)
+    elif os.path.isdir(directory):
+       print("working")
+       for elt in os.listdir(directory):
+            createEntries(f"{directory}/{elt}")
+            print(f"{directory}/{elt}")
+            
+    
+createEntries("test")
 
 def createTree(entries):
-  #an array of entries in the format [(mode, name, blobfile)]
-  entries.sort(Key=lambda x:x[1])  
-  
-  
-  tree_node = b""
-  for mode, name, blob_hex in entries:
-    blob_bytes = bytes.fromhex(blob_hex)
-    tree_node += f"{mode} {name}/\0".encode("utf-8") + blob_bytes
-    
-    
-  header = f"tree {len(tree_node)}\0".encode("utf-8")
-  full_tree = header + tree_node
-  return  hashlib.sha1(full_tree).hexdigest()
+    # an array of entries in the format [(mode, name, blobfile)]
+    entries.sort(Key=lambda x: x[1])
+
+    tree_node = b""
+    for mode, name, blob_hex in entries:
+        blob_bytes = bytes.fromhex(blob_hex)
+        tree_node += f"{mode} {name}/\0".encode("utf-8") + blob_bytes
+    header = f"tree {len(tree_node)}\0".encode("utf-8")
+    full_tree = header + tree_node
+    return hashlib.sha1(full_tree).hexdigest()
+
 
 """
 def file_digest(fileObj, digest_type):
